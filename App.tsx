27534e66abcba32ad7fc8e6fea16f2935fec3014
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2024-06-04 14:58:18 -0400

    Expo Project Setup + Map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} />
+      <View style={styles.controlsContainer}></View>
     </View>
   );
 }
@@ -13,8 +14,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightcyan",
+  },
+  controlsContainer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    flex: 0.7,
   },
 });
